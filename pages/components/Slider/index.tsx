3b8e37f3af64786d89022a4d172de4f714f39bee
Maciej Rumiński <maciejruminski91@gmail.com>
2022-03-10 13:54:39 +0100

    Moving sliderData to separate file

--- pages/components/Slider/index.tsx
+++ pages/components/Slider/index.tsx
@@ -1,39 +1,10 @@
 import Slide from "./Slide";
+import sliderData from "./sliderData";
 
 export default function Slider(): JSX.Element {
-  const slidesData: SliderData = [
-    {
-      title: "Pobierz aplikację",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euturpis molestie, dictum est a, mattis tellus. Sed dignissim, metusnec fringilla accumsan, risus sem sollicitudin lacus, ut interdumtellus elit sed risus.",
-      buttonText: "Pobierz aplikację",
-      iconSrc: "/images/icons/icon-1.svg",
-      iconAlt: "lorem ipsum",
-      mockupSrc: "/images/mockups/mockup-1.png",
-      mockupAlt: "lorem ipsum",
-    },
-    {
-      title: "Porozmawiaj z lekarzem",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euturpis molestie, dictum est a, mattis tellus. Sed dignissim, metusnec fringilla accumsan, risus sem sollicitudin lacus, ut interdumtellus elit sed risus.",
-      buttonText: "Pobierz aplikację",
-      iconSrc: "/images/icons/icon-2.svg",
-      iconAlt: "lorem ipsum",
-      mockupSrc: "/images/mockups/mockup-2.png",
-      mockupAlt: "lorem ipsum",
-    },
-    {
-      title: "Otrzymaj receptę",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euturpis molestie, dictum est a, mattis tellus. Sed dignissim, metusnec fringilla accumsan, risus sem sollicitudin lacus, ut interdumtellus elit sed risus.",
-      buttonText: "Pobierz aplikację",
-      iconSrc: "/images/icons/icon-3.svg",
-      iconAlt: "lorem ipsum",
-      mockupSrc: "/images/mockups/mockup-3.png",
-      mockupAlt: "lorem ipsum",
-    },
-  ];
-
   return (
     <div>
-      {slidesData.map((slide, index) => (
+      {sliderData.map((slide, index) => (
         <Slide data={slide} key={index} />
       ))}
     </div>
---
+++ pages/components/Slider/sliderData.tsx
@@ -0,0 +1,31 @@
+const sliderData: SliderData = [
+  {
+    title: "Pobierz aplikację",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euturpis molestie, dictum est a, mattis tellus. Sed dignissim, metusnec fringilla accumsan, risus sem sollicitudin lacus, ut interdumtellus elit sed risus.",
+    buttonText: "Pobierz aplikację",
+    iconSrc: "/images/icons/icon-1.svg",
+    iconAlt: "lorem ipsum",
+    mockupSrc: "/images/mockups/mockup-1.png",
+    mockupAlt: "lorem ipsum",
+  },
+  {
+    title: "Porozmawiaj z lekarzem",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euturpis molestie, dictum est a, mattis tellus. Sed dignissim, metusnec fringilla accumsan, risus sem sollicitudin lacus, ut interdumtellus elit sed risus.",
+    buttonText: "Pobierz aplikację",
+    iconSrc: "/images/icons/icon-2.svg",
+    iconAlt: "lorem ipsum",
+    mockupSrc: "/images/mockups/mockup-2.png",
+    mockupAlt: "lorem ipsum",
+  },
+  {
+    title: "Otrzymaj receptę",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euturpis molestie, dictum est a, mattis tellus. Sed dignissim, metusnec fringilla accumsan, risus sem sollicitudin lacus, ut interdumtellus elit sed risus.",
+    buttonText: "Pobierz aplikację",
+    iconSrc: "/images/icons/icon-3.svg",
+    iconAlt: "lorem ipsum",
+    mockupSrc: "/images/mockups/mockup-3.png",
+    mockupAlt: "lorem ipsum",
+  },
+];
+
+export default sliderData;
