fc6332ebd53662fd3a19ead0699f2f7be269de23
Maciej Rumiński <maciejruminski91@gmail.com>
2022-03-10 14:22:36 +0100

    Adding initial styles to slides

--- pages/components/Slider/Slide/index.tsx
+++ pages/components/Slider/Slide/index.tsx
@@ -5,9 +5,13 @@ export default function Slide({ data }: { data: Slide }): JSX.Element {
   return (
     <div>
       <img src={iconSrc} alt={iconAlt} />
-      <h2>{title}</h2>
-      <p>{text}</p>
-      <button>{buttonText}</button>
+      <h2 className="font-bold text-2xl mt-[56px] mb-[22px] md:mb-[33px] md:text-5xl">
+        {title}
+      </h2>
+      <p className="md:text-2xl font-semibold">{text}</p>
+      <button className="bg-black text-white py-[8px] px-[16px] rounded-sm mt-[24px] md:mt-[36px]">
+        {buttonText}
+      </button>
       <img src={mockupSrc} alt={mockupAlt} />
     </div>
   );
