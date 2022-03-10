3534e5a24584ff9418e5507251913903e69a83a3
Maciej Rumiński <maciejruminski91@gmail.com>
2022-03-10 13:42:28 +0100

    Creating slides markup more programatically based on data

--- pages/components/Slider/index.tsx
+++ pages/components/Slider/index.tsx
@@ -1,42 +1,45 @@
 export default function Slider(): JSX.Element {
+  const slidesData = [
+    {
+      title: "Pobierz aplikację",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euturpis molestie, dictum est a, mattis tellus. Sed dignissim, metusnec fringilla accumsan, risus sem sollicitudin lacus, ut interdumtellus elit sed risus.",
+      buttonText: "Pobierz aplikację",
+      iconSrc: "/images/icons/icon-1.svg",
+      iconAlt: "lorem ipsum",
+      mockupSrc: "/images/mockups/mockup-1.png",
+      mockupAlt: "lorem ipsum",
+    },
+    {
+      title: "Porozmawiaj z lekarzem",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euturpis molestie, dictum est a, mattis tellus. Sed dignissim, metusnec fringilla accumsan, risus sem sollicitudin lacus, ut interdumtellus elit sed risus.",
+      buttonText: "Pobierz aplikację",
+      iconSrc: "/images/icons/icon-2.svg",
+      iconAlt: "lorem ipsum",
+      mockupSrc: "/images/mockups/mockup-2.png",
+      mockupAlt: "lorem ipsum",
+    },
+    {
+      title: "Otrzymaj receptę",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euturpis molestie, dictum est a, mattis tellus. Sed dignissim, metusnec fringilla accumsan, risus sem sollicitudin lacus, ut interdumtellus elit sed risus.",
+      buttonText: "Pobierz aplikację",
+      iconSrc: "/images/icons/icon-3.svg",
+      iconAlt: "lorem ipsum",
+      mockupSrc: "/images/mockups/mockup-3.png",
+      mockupAlt: "lorem ipsum",
+    },
+  ];
+
   return (
     <div>
-      <div>
-        <img src="/images/icons/icon-1.svg" alt="" />
-        <h2>Pobierz aplikację</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
-          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
-          elit sed risus.
-        </p>
-        <button>Pobierz aplikację</button>
-        <img src="/images/mockups/mockup-1.png" alt="" />
-      </div>
-      <div>
-        <img src="/images/icons/icon-2.svg" alt="" />
-        <h2>Porozmawiaj z lekarzem</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
-          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
-          elit sed risus.
-        </p>
-        <button>Pobierz aplikację</button>
-        <img src="/images/mockups/mockup-2.png" alt="" />
-      </div>
-      <div>
-        <img src="/images/icons/icon-3.svg" alt="" />
-        <h2>Otrzymaj receptę</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
-          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
-          elit sed risus.
-        </p>
-        <button>Pobierz aplikację</button>
-        <img src="/images/mockups/mockup-3.png" alt="" />
-      </div>
+      {slidesData.map((slide, index) => (
+        <div key={index}>
+          <img src={slide.iconSrc} alt={slide.iconAlt} />
+          <h2>{slide.title}</h2>
+          <p>{slide.text}</p>
+          <button>{slide.buttonText}</button>
+          <img src={slide.mockupSrc} alt={slide.mockupAlt} />
+        </div>
+      ))}
     </div>
   );
 }
