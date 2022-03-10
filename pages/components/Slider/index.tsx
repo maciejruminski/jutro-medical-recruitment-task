ba3915729d05be9564b8dc1b2d14ab41b08bc834
Maciej Rumiński <maciejruminski91@gmail.com>
2022-03-10 13:29:31 +0100

    Creating initial markup for Slider component

---
+++ pages/components/Slider/index.tsx
@@ -0,0 +1,36 @@
+export default function Slider(): JSX.Element {
+  return (
+    <div>
+      <div>
+        <h2>Pobierz aplikację</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
+          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
+          elit sed risus.
+        </p>
+        <button>Pobierz aplikację</button>
+      </div>
+      <div>
+        <h2>Porozmawiaj z lekarzem</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
+          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
+          elit sed risus.
+        </p>
+        <button>Pobierz aplikację</button>
+      </div>
+      <div>
+        <h2>Otrzymaj receptę</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
+          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
+          elit sed risus.
+        </p>
+        <button>Pobierz aplikację</button>
+      </div>
+    </div>
+  );
+}
