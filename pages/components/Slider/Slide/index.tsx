b2496d5369f1ef8da996fdec2898a6791b28123e
Maciej Rumiński <maciejruminski91@gmail.com>
2022-03-10 14:48:59 +0100

    Adding state of active slide

--- pages/components/Slider/Slide/index.tsx
+++ pages/components/Slider/Slide/index.tsx
@@ -3,16 +3,21 @@ import slideColors from "./slidesColors";
 export default function Slide({
   data,
   index,
+  activeSlideIndex,
 }: {
   data: Slide;
   index: number;
+  activeSlideIndex: number;
 }): JSX.Element {
   const { iconSrc, iconAlt, title, text, buttonText, mockupSrc, mockupAlt } =
     data;
   const slideColor = slideColors[index];
+  const isActive = activeSlideIndex === index ? "opacity-100" : "opacity-0";
 
   return (
-    <div className={`${slideColor.bg} ${slideColor.color}`}>
+    <div
+      className={`${slideColor.bg} ${slideColor.color} absolute top-0 left-0 w-full ${isActive}`}
+    >
       <img src={iconSrc} alt={iconAlt} />
       <h2 className="font-bold text-2xl mt-[56px] mb-[22px] md:mb-[33px] md:text-5xl">
         {title}
--- pages/components/Slider/index.tsx
+++ pages/components/Slider/index.tsx
@@ -1,11 +1,20 @@
 import Slide from "./Slide";
 import sliderData from "./sliderData";
 
+import { useState } from "react";
+
 export default function Slider(): JSX.Element {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+
   return (
-    <div>
+    <div className="relative">
       {sliderData.map((slide, index) => (
-        <Slide data={slide} index={index} key={index} />
+        <Slide
+          data={slide}
+          index={index}
+          activeSlideIndex={activeSlideIndex}
+          key={index}
+        />
       ))}
     </div>
   );
