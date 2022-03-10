8d955a6fcafc0a2ef38f3eda62b093c6b415923f
Maciej Rumiński <maciejruminski91@gmail.com>
2022-03-10 18:20:30 +0100

    Moving components directory out of pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import Slider from "./components/Slider";
+import Slider from "../components/Slider";
 
 const Home: NextPage = () => {
   return (
